275122c2931330222465c304e98d582f07f0aac9
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2026-01-11 18:42:33 -0800

    Initial Commit

---
+++ popcorn/popcornApp.swift
@@ -0,0 +1,17 @@
+//
+//  popcornApp.swift
+//  popcorn
+//
+//  Created by Enzo on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct popcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
